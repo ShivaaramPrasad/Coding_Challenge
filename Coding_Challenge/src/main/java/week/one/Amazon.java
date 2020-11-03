8399c9e96bbde65974bf671ac23e9d918f86a3ba
DELL <DELL@192.168.1.12>
2020-11-03 17:25:05 +0530

    updated 2nday code

--- Coding_Challenge/src/main/java/week/one/Amazon.java
+++ Coding_Challenge/src/main/java/week/one/Amazon.java
@@ -17,47 +17,61 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Amazon {
 
-	public static void main(String[] args) {
-		        // Lunch  Chrome 
-				ChromeOptions options = new ChromeOptions();
-				options.addArguments("--disable-notifications");
-				WebDriverManager.chromedriver().setup();
-				ChromeDriver driver = new ChromeDriver(options);
-				driver.manage().window().maximize();
+	public static void main(String[] args) throws InterruptedException {
+		// Lunch  Chrome 
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
 
-				//Load URL:
-				//	    1. Login to https://www.amazon.in/
+		//Load URL:
+		//	    1. Login to https://www.amazon.in/
 
-				driver.get("https://www.amazon.in/");
-				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-				
-				// Click Choose and Select Furniture
-				driver.findElement(By.id("searchDropdownBox")).click();
-				
-				WebElement allCategories =driver.findElement(By.id("searchDropdownBox"));
-				Select value_one = new Select(allCategories);
-				List<WebElement> options_two=value_one.getOptions();
-				//int count =options.size();
-				value_one.selectByVisibleText("Furniture");
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("Search for costliest furniture");
+		// Click Choose and Select Furniture
+		driver.findElement(By.id("searchDropdownBox")).click();
 
-				// Type 'chairs for computer table' and Enter
-				driver.findElement(By.id("twotabsearchtextbox")).sendKeys("chairs for computer table",Keys.ENTER);
-				
-				List<WebElement> price = driver.findElements(By.xpath("(//span[text()='Sponsored'])[2]/following::span[@class='a-price']"));
-				
-				List<String> a = new ArrayList<String>();
+		WebElement allCategories =driver.findElement(By.id("searchDropdownBox"));
+		Select value_one = new Select(allCategories);
+		List<WebElement> options_two=value_one.getOptions();
+		//int count =options.size();
+		value_one.selectByVisibleText("Furniture");
 
-				for (WebElement webElement_sort : price) {
-					String namesorted = webElement_sort.getText().replaceAll("[^0-9]", "").replaceAll(" ", "");
-					a.add(namesorted);
-					Collections.sort(a);
-					Collections.reverse(a);
+		// Type 'chairs for computer table' and Enter
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("chairs for computer table",Keys.ENTER);
 
-				}
-				System.out.println("The costliest price of the Furniture ₹"+a.get(0)); 
+		List<WebElement> price = driver.findElements(By.xpath("(//span[text()='Sponsored'])[2]/following::span[@class='a-price']"));
 
-				}
+		List<String> a = new ArrayList<String>();
 
+		for (WebElement webElement_sort : price) {
+			String namesorted = webElement_sort.getText();
+			a.add(namesorted);
+			Collections.sort(a);
+			Collections.reverse(a);
+
+		}
+		String costilest=a.get(0);
+		System.out.println("The costliest price of the Furniture "+costilest); 
+		driver.findElement(By.xpath("//span[text()='"+costilest+"']/preceding::i[2]")).click();
+		String globalRatings = driver.findElement(By.xpath("//span[contains(text(),'global ratings')]")).getText();
+		System.out.println(globalRatings+" for the costilest Furniture ");
+		String globalRatingsText=globalRatings.replaceAll("[^0-9]", "");
+		int globalRatingsNum =Integer.parseInt(globalRatingsText);
+		String stars = driver.findElement(By.xpath("//span[text()='"+globalRatings+"']/following::a")).getText();
+		String percenatage = driver.findElement(By.xpath("(//span[text()='"+globalRatings+"']/following::a)[3]")).getText();
+		System.out.println(stars+" for the costilest furniture is "+percenatage);
+		String percenatageText=percenatage.replaceAll("[^0-9]", "");
+		int percenatageNum =Integer.parseInt(percenatageText);
+		int cal=globalRatingsNum*percenatageNum/100;
+		System.out.println("The costliest the Furniture "+stars+"* rating is "+percenatageNum+ "% of " +globalRatingsNum+"= "+cal);
+        Thread.sleep(5000);
+        driver.close();
 	}
+
+}
 
 
